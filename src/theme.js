04675c304289551d907b5936ac0c148e55d44dd5
martinwheeler <martin@martinwheeler.com.au>
2018-11-21 22:02:05 +1000

    Add theme file

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+export const PADDING = {
+  S: 5,
+  M: 10,
+  L: 15,
+  XL: 20
+};
+
+export const FONT = {
+  SIZE: {
+    XXS: 10,
+    XS: 12,
+    S: 14,
+    M: 16,
+    XL: 20
+  },
+  WEIGHT: {
+    NORMAL: "400",
+    SEMI_BOLD: "600",
+    BOLD: "900"
+  }
+};
